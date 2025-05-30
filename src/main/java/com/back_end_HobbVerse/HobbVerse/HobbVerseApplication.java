90a057a78e1143acdeeaee4f236924f3325f29d7
Diana <arq.dianamendez94@gmail.com>
2025-05-30 15:54:22 -0500

    Creación back End base Hobbverse

---
+++ src/main/java/com/back_end_HobbVerse/HobbVerse/HobbVerseApplication.java
@@ -0,0 +1,13 @@
+package com.back_end_HobbVerse.HobbVerse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbVerseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HobbVerseApplication.class, args);
+	}
+
+}
