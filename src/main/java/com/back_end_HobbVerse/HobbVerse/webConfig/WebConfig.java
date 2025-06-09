ab91f4854b22299d5434939b1dfadebde5ae98df
Diana <arq.dianamendez94@gmail.com>
2025-06-09 13:35:09 -0500

    Web config

---
+++ src/main/java/com/back_end_HobbVerse/HobbVerse/webConfig/WebConfig.java
@@ -0,0 +1,24 @@
+package com.back_end_HobbVerse.HobbVerse.webConfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000, https://dev.d3n6cwsyyj1ifl.amplifyapp.com/") // URL local y la URL desplegada AWS
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
